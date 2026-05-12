4636d9f74d8f1c41b1b38d2350eac333d2af734c
Rhenzo27 <jefersonsam0@gmail.com>
2026-05-12 15:28:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
+// =========================
+// SCRIPT.JS
+// =========================
+
 window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.navbar');
 
   if (window.scrollY > 50) {
     navbar.style.background = 'rgba(15, 23, 42, 0.95)';
   } else {
-    navbar.style.background = 'rgba(15, 23, 42, 0.6)';
+    navbar.style.background = 'rgba(15, 23, 42, 0.65)';
   }
-});
-
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(button => {
-  button.addEventListener('mouseenter', () => {
-    button.style.transform = 'scale(1.05)';
-  });
-
-  button.addEventListener('mouseleave', () => {
-    button.style.transform = 'scale(1)';
-  });
 });
